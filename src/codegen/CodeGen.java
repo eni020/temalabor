7e0c5a7f9314efb505c828766856729a27b9449c
eni020 <47690361+eni020@users.noreply.github.com>
2019-12-01 21:41:03 +0100

    végleges

--- src/codegen/CodeGen.java
+++ src/codegen/CodeGen.java
@@ -12,6 +12,49 @@ public class CodeGen {
     private static List<Sourcecode> sourcecodes = new ArrayList<>();
     static int i = 0;
 
+    public static void containsNew(String[] line) {
+        if (line[3].contains("Parameter")) {
+            String unit = line[3].substring(line[3].indexOf('(') + 1, line[3].indexOf(','));
+            String value = line[4].substring(0, line[4].indexOf(')'));
+            m.setParameters(line[0], unit, value);
+        }
+        if (line[3].contains("Variable")) {
+            String value = "";
+            if (line[3].contains("getValue")) {
+                value = line[3].substring(line[3].indexOf('(') + 1, line[3].indexOf('.'));
+            } else {
+                value = line[3].substring(line[3].indexOf('(') + 1, line[3].indexOf(')'));
+            }
+            m.setVariables(line[0], value);
+        }
+    }
+
+
+    public static void containsDer(String[] line) {
+        String value = "";
+        String var = line[0].substring(line[0].indexOf('(') + 1, line[0].indexOf(','));
+        if(line[1].contains("new")) {
+            value = line[2].substring(line[2].indexOf('(') + 1, line[2].indexOf(')'));
+        }
+        else {
+            value = line[1].substring(0, line[1].indexOf(')'));
+        }
+        m.setEquations( "der(" + var + ") = " + value + ";");
+    }
+
+
+
+    public static void containsIf(String[] line) {
+        String when = "";
+        String reinit = "";
+        if (line[0].contains("getValue")) {
+            when = "when (" + line[0].substring(line[0].indexOf('(') + 1, line[0].indexOf('.')) + " " + line[1] + " " + line[2] +  " then ";
+        }
+        if (line[4].contains("setValue")) {
+            reinit = "reinit(" + line[4].substring(0, line[4].indexOf('.')) + ", " + line[4].substring(line[4].indexOf('(') + 1, line[4].length()) +  " end when;";
+        }
+        m.setEquations(when + reinit);
+    }
     public static void line() {
         for (Sourcecode source: sourcecodes){
             List<String[]> src = source.getSource();
@@ -21,42 +64,13 @@ public class CodeGen {
                         m = new ModelGen(line[i + 1]);
                     }
                     if(line[i].contains("new") && !line[2].contains("Variable")) {
-                       if (line[3].contains("Parameter")) {
-                            String unit = line[3].substring(line[3].indexOf('(') + 1, line[3].indexOf(','));
-                            String value = line[4].substring(0, line[4].indexOf(')'));
-                            m.setParameters(line[0], unit, value);
-                        }
-                        if (line[3].contains("Variable")) {
-                            String value = "";
-                            if (line[3].contains("getValue")) {
-                                value = line[3].substring(line[3].indexOf('(') + 1, line[3].indexOf('.'));
-                            } else {
-                                value = line[3].substring(line[3].indexOf('(') + 1, line[3].indexOf(')'));
-                            }
-                            m.setVariables(line[0], value);
-                        }
+                        containsNew(line);
                     }
                     if(line[i].contains("der")) {
-                        String value = "";
-                        String var = line[0].substring(line[0].indexOf('(') + 1, line[0].indexOf(','));
-                        if(line[1].contains("new")) {
-                            value = line[2].substring(line[2].indexOf('(') + 1, line[2].indexOf(')'));
-                        }
-                        else {
-                            value = line[1].substring(0, line[1].indexOf(')'));
-                        }
-                        m.setEquations( "der(" + var + ") = " + value + ";");
+                        containsDer(line);
                     }
                     if(line[i].contains("if")) {
-                        String when = "";
-                        String reinit = "";
-                        if (line[0].contains("getValue")) {
-                            when = "when (" + line[0].substring(line[0].indexOf('(') + 1, line[0].indexOf('.')) + " " + line[1] + " " + line[2] +  " then ";
-                        }
-                        if (line[4].contains("setValue")) {
-                            reinit = "reinit(" + line[4].substring(0, line[4].indexOf('.')) + ", " + line[4].substring(line[4].indexOf('(') + 1, line[4].length()) +  " end when;";
-                        }
-                            m.setEquations(when + reinit);
+                        containsIf(line);
                     }
                 }
             }
@@ -84,9 +98,7 @@ public class CodeGen {
                     line = linehelper.toArray(String[]::new);
                     source.add(line);
                 }
-
             }
-            List<String[]> src = source.getSource();
             br.close();
             sourcecodes.add(source);
         } catch (Exception e) {
